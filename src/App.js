6b867d77c219c46b746b09cc96386485f84ecd72
jkeen95 <jkeen95@gmail.com>
2022-09-11 19:36:08 -0400

    Editing Login and Sign Up Functionality

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import './App.css';
+import { Amplify } from 'aws-amplify';
+
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
+function App() {
+  return (
+    <Authenticator signUpAttributes={[
+          'birthdate',
+          'email',
+          'name',
+        ]}>
+          {({ signOut, user }) => (
+            <div className="App">
+                  <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                      Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <a
+                      className="App-link"
+                      href="https://reactjs.org"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Learn React
+                    </a>
+                  </header>
+                </div>
+          )}
+        </Authenticator>
+  );
+}
+
+export default App;
