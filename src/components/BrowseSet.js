9e5c4c3b2692e2f021ecc2d4cd62767a58eb304c
jkeen95 <jkeen95@gmail.com>
2022-10-28 08:24:31 -0400

    props test6

--- src/components/BrowseSet.js
+++ src/components/BrowseSet.js
@@ -274,12 +274,12 @@ class BrowseSet extends React.Component {
         console.log("rendereee " + JSON.stringify(this.state))
            if(this.state.flashcardsToBrowse.length !== 0) {
             //console.log("inside")
-            // const frontHeader = this.state.showSetTitleSide ? "Title" : "Definition"
-            // const backHeader = this.state.showSetTitleSide ? "Definition" : "Title"
-            // const editUrl = "" + window.location.origin +"/set/" + this.state.setInfo.flashSetId + "/edit"
-            // const memorizeUrl = "" + window.location.origin + "/set/" + this.state.setInfo.flashSetId + "/memorize";
-            // const front = this.state.showSetTitleSide ? this.state.flashcardsToBrowse[this.state.index].title : this.state.flashcardsToBrowse[this.state.index].definition;
-            // const back = this.state.showSetTitleSide ? this.state.flashcardsToBrowse[this.state.index].definition : this.state.flashcardsToBrowse[this.state.index].title;
+            const frontHeader = this.state.showSetTitleSide ? "Title" : "Definition"
+            const backHeader = this.state.showSetTitleSide ? "Definition" : "Title"
+            const editUrl = "" + window.location.origin +"/set/" + this.state.setInfo.flashSetId + "/edit"
+            const memorizeUrl = "" + window.location.origin + "/set/" + this.state.setInfo.flashSetId + "/memorize";
+            const front = this.state.showSetTitleSide ? this.state.flashcardsToBrowse[this.state.index].title : this.state.flashcardsToBrowse[this.state.index].definition;
+            const back = this.state.showSetTitleSide ? this.state.flashcardsToBrowse[this.state.index].definition : this.state.flashcardsToBrowse[this.state.index].title;
             //console.log("before return")
             return (
                 <div>
@@ -287,31 +287,31 @@ class BrowseSet extends React.Component {
                     <br />
                     <hr />
                     <br />
-                    {/*<h4>ID: {this.state.setInfo.flashSetId}</h4>*/}
-                    {/*<h4>Name: {this.state.setInfo.flashSetName}</h4>*/}
-                    {/*<h4>Description: {this.state.setInfo.flashSetDescription}</h4>*/}
-                    {/*<h4>Visibility: {this.state.setInfo.flashSetVisibility}</h4>*/}
-                    {/*<br />*/}
-                    {/*<a href={editUrl}>Edit</a>*/}
-                    {/*<br/>*/}
-                    {/*<a href={memorizeUrl}>Memorize</a>*/}
-                    {/*<br />*/}
-                    {/*<hr />*/}
-                    {/*<div>*/}
-                    {/*    <input type="radio" value="Title" name="Title" checked={this.state.showSetTitleSide} onChange={this.radioChange}/> Title*/}
-                    {/*    <input type="radio" value="Definition" name="Definition" checked={!this.state.showSetTitleSide} onChange={this.radioChange}/> Definition*/}
-                    {/*</div>*/}
-                    {/*<div>*/}
-                    {/*    <SwitchField label="Randomize" onChange={() => this.randomOrderToggle()} />*/}
-                    {/*</div>*/}
-                    {/*<br />*/}
-                    {/*<p>{this.state.currentCardOnFront ? frontHeader : backHeader}</p>*/}
-                    {/*<p>{this.state.index+1} / {this.state.setInfo.titles.length}</p>*/}
-                    {/*<FlipFlashcard key={this.state.index} front={front} back={back} frontSide={this.state.currentCardOnFront} swapSide={this.swapSide}/>*/}
-                    {/*<div className="card_buttons">*/}
-                    {/*    <div className="prev_button" onClick={() => {this.previousCard()}}>Previous</div>*/}
-                    {/*    <div className="next_button" onClick={() => {this.nextCard()}}>Next</div>*/}
-                    {/*</div>*/}
+                    <h4>ID: {this.state.setInfo.flashSetId}</h4>
+                    <h4>Name: {this.state.setInfo.flashSetName}</h4>
+                    <h4>Description: {this.state.setInfo.flashSetDescription}</h4>
+                    <h4>Visibility: {this.state.setInfo.flashSetVisibility}</h4>
+                    <br />
+                    <a href={editUrl}>Edit</a>
+                    <br/>
+                    <a href={memorizeUrl}>Memorize</a>
+                    <br />
+                    <hr />
+                    <div>
+                        <input type="radio" value="Title" name="Title" checked={this.state.showSetTitleSide} onChange={this.radioChange}/> Title
+                        <input type="radio" value="Definition" name="Definition" checked={!this.state.showSetTitleSide} onChange={this.radioChange}/> Definition
+                    </div>
+                    <div>
+                        <SwitchField label="Randomize" onChange={() => this.randomOrderToggle()} />
+                    </div>
+                    <br />
+                    <p>{this.state.currentCardOnFront ? frontHeader : backHeader}</p>
+                    <p>{this.state.index+1} / {this.state.setInfo.titles.length}</p>
+                    <FlipFlashcard key={this.state.index} front={front} back={back} frontSide={this.state.currentCardOnFront} swapSide={this.swapSide}/>
+                    <div className="card_buttons">
+                        <div className="prev_button" onClick={() => {this.previousCard()}}>Previous</div>
+                        <div className="next_button" onClick={() => {this.nextCard()}}>Next</div>
+                    </div>
                 </div>
             )
          }
