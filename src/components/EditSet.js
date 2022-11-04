6518d383b2d3b37bec6f3a09bbc1b01c947c36c7
jkeen95 <jkeen95@gmail.com>
2022-11-03 21:15:37 -0400

    Conflict Resolution change

--- src/components/EditSet.js
+++ src/components/EditSet.js
@@ -60,29 +60,32 @@ class EditSet extends React.Component {
 
 
         console.log("before ----- " + JSON.stringify(setToSave))
-        const original = await DataStore.query(FlashcardSet, (set) =>
+        // const original = await DataStore.query(FlashcardSet, (set) =>
+        //     set.id('eq', this.props.setId.id).owner('eq', this.props.currentUser.username)
+        // );
+        await DataStore.query(FlashcardSet, (set) =>
             set.id('eq', this.props.setId.id).owner('eq', this.props.currentUser.username)
-        );
-        await DataStore.save(
-            FlashcardSet.copyOf(original[0], updated => {
-                updated.name = setToSave.flashSetName;
-                updated.description = setToSave.flashSetDescription;
-                updated.visibility = setToSave.flashSetVisibility;
-                updated.titles = setToSave.titles;
-                updated.definitions =  setToSave.definitions;
-            })
-        ).then(result => {
-            console.log(JSON.stringify("test ----" + JSON.stringify(result)))
-            alert('A flashcard set was saved: ' + result.name + "\n" +
-                'A visibility was saved: ' + result.visibility + "\n" +
-                'A description was saved: ' + result.description + "\n" +
-                'FlashcardInput titles were saved: ' + result.titles + "\n" +
-                'FlashcardInput definitions were saved: ' + result.definitions + "\n"
-            );
-            const url = "" + window.location.origin + "/set/" + result.id + "/browse";
-            //console.log(url)
-            window.location.replace(url)
-        });
+        ).then(original => {
+            DataStore.save(
+                FlashcardSet.copyOf(original[0], updated => {
+                    updated.name = setToSave.flashSetName;
+                    updated.description = setToSave.flashSetDescription;
+                    updated.visibility = setToSave.flashSetVisibility;
+                    updated.titles = setToSave.titles;
+                    updated.definitions =  setToSave.definitions;
+                })
+            ).then(result => {
+                console.log(JSON.stringify("test ----" + JSON.stringify(result)))
+                alert('A flashcard set was saved: ' + result.name + "\n" +
+                    'A visibility was saved: ' + result.visibility + "\n" +
+                    'A description was saved: ' + result.description + "\n" +
+                    'FlashcardInput titles were saved: ' + result.titles + "\n" +
+                    'FlashcardInput definitions were saved: ' + result.definitions + "\n"
+                );
+                const url = "" + window.location.origin + "/set/" + result.id + "/browse";
+                //console.log(url)
+                window.location.replace(url)
+            })});
     }
 
     render() {
