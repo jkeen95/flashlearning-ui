c9ad0acb46b375b9b21533613f5ffa5c8be566d8
jkeen95 <jkeen95@gmail.com>
2022-11-12 22:50:47 -0500

    Added Results for Memorization Without Repetition

--- src/components/MemorizeSet.js
+++ src/components/MemorizeSet.js
@@ -17,7 +17,9 @@ class MemorizeSet extends React.Component {
             randomizeOn: true,
             correctCounter: 0,
             incorrectCounter: 0,
-            sessionFinished: false
+            sessionFinished: false,
+            incorrectCountArray: [],
+            correctCorrectArray: [],
         };
     }
 
@@ -35,6 +37,24 @@ class MemorizeSet extends React.Component {
     }
 
     incorrect = async () => {
+        if(this.state.withRepetition){
+
+        }
+        else {
+            await this.withoutRepetitionIncorrect()
+        }
+    }
+
+    withoutRepetitionIncorrect = async () => {
+        let originalIndex
+        if(this.state.originalOrder) {
+            originalIndex = this.state.index
+        }
+        else {
+            originalIndex = await this.findOriginalIndex()
+        }
+        this.state.incorrectCountArray[originalIndex] = this.state.incorrectCountArray[originalIndex] + 1;
+        console.log(this.state.incorrectCountArray)
         await this.setState({
             incorrectCounter: this.state.incorrectCounter + 1
         })
@@ -42,10 +62,29 @@ class MemorizeSet extends React.Component {
     }
 
     correct = async () => {
+        if(this.state.withRepetition){
+
+        }
+        else {
+            await this.withoutRepetitionCorrect()
+        }
+    }
+
+    withoutRepetitionCorrect = async () => {
         await this.setState({
             correctCounter: this.state.correctCounter + 1
         })
         await this.incrementIndex()
+    }
+
+    findOriginalIndex = async () => {
+        const currentCard = this.state.flashcardsToBrowse[this.state.index]
+        console.log(currentCard)
+        const originalIndex = this.state.originalOrderFlashcards.findIndex(card => {
+            return card.title === currentCard.title
+        })
+        console.log(originalIndex)
+        return originalIndex
     }
 
     incrementIndex = async () => {
@@ -75,7 +114,9 @@ class MemorizeSet extends React.Component {
         console.log(originalOrder)
         await this.setState({
             flashcardsToBrowse: originalOrder,
-            originalOrderFlashcards: JSON.parse(JSON.stringify(originalOrder))
+            originalOrderFlashcards: JSON.parse(JSON.stringify(originalOrder)),
+            incorrectCountArray: Array(originalOrder.length).fill(0),
+            correctCorrectArray: Array(originalOrder.length).fill(0),
         })
         console.log("original " + JSON.stringify(this.state));
     }
@@ -102,13 +143,29 @@ class MemorizeSet extends React.Component {
         console.log("random " + JSON.stringify(this.state.flashcardsToBrowse));
     }
 
+    printIncorrectGuesses = () => {
+        const incorrectGuesses = this.state.incorrectCountArray.map((incorrectCount, index) => {
+            if(incorrectCount > 0) {
+                return <p>Card #{index+1} - {incorrectCount} {incorrectCount === 1 ? "time" : "times"}</p>
+            }
+        })
+        return incorrectGuesses
+        console.log(incorrectGuesses)
+    }
+
     render() {
         //console.log(JSON.stringify(this.props.setInfo))
         if(this.state.flashcardsToBrowse.length !== 0) {
             //console.log("inside ---" + JSON.stringify(this.state))
             if(this.state.sessionFinished) {
                 return (
-                    <h1>Finished!</h1>
+                    <div>
+                        <h1>Finished Studying the {this.props.titleSide ? "Title" : "Definition"} side of the Flashcard Set, {this.props.setInfo.name}, in {this.props.originalOrder ? "Original" : "Randomized"} order {this.props.withRepetition ? "with" : "without"} Repetition!</h1>
+                        <h2>Results</h2>
+                        <h3>Correct Percentage: {((this.state.originalOrderFlashcards.length - this.state.incorrectCounter) / this.state.originalOrderFlashcards.length) * 100}%</h3>
+                        <h3>Incorrect Guesses: {this.state.incorrectCounter}</h3>
+                        <h4>{this.printIncorrectGuesses()}</h4>
+                    </div>
                 )
             }
             const frontHeader = this.state.showSetTitleSide ? "Title" : "Definition"
